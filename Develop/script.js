e03702a832ac73555b316ec73228b0b4c987b8a1
rhubarb414 <ctophr.dubois@gmail.com>
2023-01-17 21:06:54 -0800

    retrieves and displays local storage tasks upon page load

--- Develop/script.js
+++ Develop/script.js
@@ -19,7 +19,6 @@ var allTasks = {
 $(function () {
   var todayEl = document.getElementById("currentDay");
 
-  var agenda = {};
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -27,6 +26,7 @@ $(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
 
+  //add listener to run clickHandler() on each timeblock
   timeBlockEls.forEach(clickHandler);
 
   function clickHandler(timeBlock) {
@@ -34,19 +34,17 @@ $(function () {
     timeBlock.addEventListener("click", function (event) {
       var task = this.querySelector(".description").value; //assign task the input in the time block's text area
       var clickTarget = event.target;
-      var currentTimeBlock = this.id;
-      console.log("currentTimeBlock = " + currentTimeBlock);
+      var currentTimeBlock = this.id; //hour value of time-block
 
-      console.log(`${this.id}`); //hour value of time-block
-      //check if save button or save button hover is clicked
+      //check if save button or hover pseudo element is clicked
       if (
         clickTarget.className === "btn saveBtn col-2 col-md-1" ||
         clickTarget.className === "fas fa-save"
       ) {
-        console.log(`Button area clicked!`);
-        console.log("task description = " + task);
         //add new task to allTasks object
         allTasks[currentTimeBlock] = task.trim();
+
+        //store updated allTasks in local storage
         localStorage.setItem("allTasks", JSON.stringify(allTasks));
       }
     });
@@ -61,6 +59,26 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+  function getSavedTasks() {
+    //get allTasks obj from local Stage
+    var savedTasks = JSON.parse(localStorage.getItem("allTasks"));
+
+    //Don't overwrite preformatted allTasks obj with null if nothing in local storage
+    if (savedTasks !== null) {
+      //otherwise update allTasks with local storage values
+      allTasks = savedTasks;
+    }
+
+    //display savedTasks in corresponding time blocks
+    for (var timeBlock in allTasks) {
+      // select <textarea> child of current time-block div
+      var textArea = $(`#${timeBlock}`).children("textarea");
+      //update textarea with savedTask
+      textArea.text(allTasks[timeBlock]);
+    }
+  }
+  getSavedTasks();
   //
   // TODO: Add code to display the current date in the header of the page.
 
